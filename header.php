061b352171bc9014e44b16c63ee1ca0a971ec403
Nelson Santos <nsantos@pombaldir.com>
2025-08-31 00:41:05 +0100

    ok

--- header.php
+++ header.php
@@ -91,36 +91,39 @@ foreach ($sidebarTypes as $sidebarType):
 
         <!-- Top navigation -->
         <div class="top_nav">
-            <div class="nav_menu">
-                <div class="nav toggle">
-                    <a id="menu_toggle"><i class="fa fa-bars"></i></a>
-                </div>
-                <nav class="nav navbar-nav">
-                    <ul class="navbar-nav ms-auto">
-
-
-                        <li class="nav-item dropdown ms-3">
-                            <a href="javascript:;" class="user-profile nav-link dropdown-toggle" aria-haspopup="true" id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
-                                <img src="assets/images/img.jpg" alt=""><?php echo htmlspecialchars($user['username']); ?>
-                            </a>
-                            <div class="dropdown-menu dropdown-menu-end dropdown-usermenu" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="<?= BASE_URL ?>editar-perfil"> Profile</a>
-                                <a class="dropdown-item" href="<?= BASE_URL ?>definicoes">
-                                    <span class="badge bg-red float-end">50%</span>
-                                    <span>Settings</span>
-                                </a>
-                                <a class="dropdown-item" href="javascript:;">Help</a>
-                                <a class="dropdown-item" href="<?= BASE_URL ?>terminar-sessao"><i class="fa fa-sign-out float-end"></i> Log Out</a>
-                            </div>
-                        </li>
-
-                    </ul>
-
-
-
-                </nav>
-            </div>
+    <div class="nav_menu">
+        <div class="nav toggle">
+            <a id="menu_toggle"><i class="fa fa-bars"></i></a>
         </div>
+
+        <!-- Barra de navegação -->
+        <nav class="d-flex w-100 align-items-center">
+            <ul class="navbar-nav ms-auto">
+                <li class="nav-item dropdown ms-3">
+                    <a href="javascript:;" class="user-profile nav-link dropdown-toggle"
+                       aria-haspopup="true" id="navbarDropdown" data-bs-toggle="dropdown"
+                       aria-expanded="false">
+                        <img src="assets/images/img.jpg" alt="">
+                        <?php echo htmlspecialchars($user['username']); ?>
+                    </a>
+                    <div class="dropdown-menu dropdown-menu-end dropdown-usermenu"
+                         aria-labelledby="navbarDropdown">
+                        <a class="dropdown-item" href="<?= BASE_URL ?>editar-perfil">Profile</a>
+                        <a class="dropdown-item" href="<?= BASE_URL ?>definicoes">
+                            <span class="badge bg-red float-end">50%</span>
+                            <span>Settings</span>
+                        </a>
+                        <a class="dropdown-item" href="javascript:;">Help</a>
+                        <a class="dropdown-item" href="<?= BASE_URL ?>terminar-sessao">
+                            <i class="fa fa-sign-out float-end"></i> Log Out
+                        </a>
+                    </div>
+                </li>
+            </ul>
+        </nav>
+    </div>
+</div>
+
         <!-- /Top navigation -->
 
 
