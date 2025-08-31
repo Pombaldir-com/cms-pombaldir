73c118f12fe1623f1d3a644a0e5625a6c5e9de7f
Nelson Santos <nsantos@pombaldir.com>
2025-08-31 12:29:51 +0100

    header

--- header.php
+++ header.php
@@ -98,11 +98,7 @@ foreach ($sidebarTypes as $sidebarType):
       </div>
 
       <ul class="navbar-nav ms-auto d-flex align-items-center">
-        <?php if (($user['role'] ?? 3) <= 2): ?>
-        <li class="nav-item">
-          <a class="nav-link" href="<?= BASE_URL ?>users"><i class="fa fa-users"></i> Utilizadores</a>
-        </li>
-        <?php endif; ?>
+
         <li class="nav-item dropdown ms-3">
           <a href="javascript:;" class="user-profile nav-link dropdown-toggle d-flex align-items-center"
              id="navbarDropdown" data-bs-toggle="dropdown" aria-expanded="false">
@@ -111,14 +107,20 @@ foreach ($sidebarTypes as $sidebarType):
           </a>
           <div class="dropdown-menu dropdown-menu-end dropdown-usermenu"
                aria-labelledby="navbarDropdown">
-            <a class="dropdown-item" href="<?= BASE_URL ?>editar-perfil">Profile</a>
+            <a class="dropdown-item" href="<?= BASE_URL ?>editar-perfil"><i class="fa fa-user"></i> Perfil</a>
+
+        <?php if (($user['role'] ?? 3) <= 2): ?>
+      
+          <a class="dropdown-item" href="<?= BASE_URL ?>users"><i class="fa fa-users"></i> Utilizadores</a>
+      
+        <?php endif; ?>
+
             <a class="dropdown-item" href="<?= BASE_URL ?>definicoes">
               <span class="badge bg-red float-end">50%</span>
-              <span>Settings</span>
-            </a>
-            <a class="dropdown-item" href="javascript:;">Help</a>
+              <i class="fa fa-cog"></i> <span>Definições</span>
+            </a>    
             <a class="dropdown-item" href="<?= BASE_URL ?>terminar-sessao">
-              <i class="fa fa-sign-out float-end"></i> Log Out
+              <i class="fa fa-sign-out float-end"></i> Terminar Sessão
             </a>
           </div>
         </li>
