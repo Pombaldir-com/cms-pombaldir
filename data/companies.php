36dd11a9aafb54c5e27f0e91f9b65e769451f41a
Nelson Santos <pombaldir@pombaldir.com>
2025-09-02 15:45:50 +0100

    Add multi-company configuration and session-based DB access

---
+++ data/companies.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    '123456789' => [
+        'db_host' => 'localhost',
+        'db_name' => 'cms_empresa1',
+        'db_user' => 'root',
+        'db_pass' => 'root',
+        'slug'    => 'empresa1',
+    ],
+    '987654321' => [
+        'db_host' => 'localhost',
+        'db_name' => 'cms_empresa2',
+        'db_user' => 'root',
+        'db_pass' => 'root',
+        'slug'    => 'empresa2',
+    ],
+];
+
