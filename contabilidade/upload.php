27354677b293e414a9d4f64d376dfaa4249113ff
Nelson Santos <pombaldir@pombaldir.com>
2025-09-04 11:29:29 +0100

    Add accounting document upload page

---
+++ contabilidade/upload.php
@@ -0,0 +1,7 @@
+<?php
+require_once __DIR__ . '/../header.php';
+?>
+<form id="multi-upload" class="dropzone"></form>
+<script src="vendors/dropzone/dropzone.min.js"></script>
+<script src="assets/js/accounting_upload.js"></script>
+<?php require_once __DIR__ . '/../footer.php'; ?>
