e652f7a65c8457a231ea875ef357522819de8890
Nelson Santos <nsantos@pombaldir.com>
2025-09-04 11:57:59 +0100

    dropzone

--- contabilidade/upload.php
+++ contabilidade/upload.php
@@ -2,6 +2,6 @@
 require_once __DIR__ . '/../header.php';
 ?>
 <form id="multi-upload" class="dropzone"></form>
-<script src="vendors/dropzone/dropzone.min.js"></script>
+<script src="vendors/dropzone/dist/dropzone-min.js"></script>
 <script src="assets/js/accounting_upload.js"></script>
 <?php require_once __DIR__ . '/../footer.php'; ?>
