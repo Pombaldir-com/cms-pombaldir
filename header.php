b8a163af082754e2d0d30fa7ff9b7933a35e0887
Nelson Santos <pombaldir@pombaldir.com>
2025-09-04 11:52:03 +0100

    feat: show accounting menu from active modules

--- header.php
+++ header.php
@@ -82,7 +82,7 @@ foreach ($sidebarTypes as $sidebarType):
                                 </ul>
                             </li>
 <?php endforeach; ?>
-<?php if (getSetting('accounting_enabled', '0') === '1'): ?>
+<?php if (isModuleActive('contabilidade')): ?>
                             <li><a href="<?= BASE_URL ?>contabilidade/upload"><i class="fa fa-upload"></i> Multi Upload</a></li>
 <?php endif; ?>
                         </ul>
