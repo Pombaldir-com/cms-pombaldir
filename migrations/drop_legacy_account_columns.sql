c9d67efb2a8de989bc38f3a3c87f8e837ce1e1f8
Nelson Santos <pombaldir@pombaldir.com>
2025-09-13 14:56:18 +0100

    refactor: drop legacy account columns via migration

---
+++ migrations/drop_legacy_account_columns.sql
@@ -0,0 +1,9 @@
+ALTER TABLE accounting_imports DROP COLUMN account_iva6;
+ALTER TABLE accounting_imports DROP COLUMN account_iva13;
+ALTER TABLE accounting_imports DROP COLUMN account_iva23;
+ALTER TABLE accounting_imports DROP COLUMN account_novat;
+
+ALTER TABLE accounting_classifications DROP COLUMN account_iva6;
+ALTER TABLE accounting_classifications DROP COLUMN account_iva13;
+ALTER TABLE accounting_classifications DROP COLUMN account_iva23;
+ALTER TABLE accounting_classifications DROP COLUMN account_novat;
